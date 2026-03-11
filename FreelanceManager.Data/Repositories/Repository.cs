bdfbffcec1ab786e5875ac28526400ca880b395a
Motasem-Ali-A <mutasem.abusarhan@gmail.com>
2026-03-11 21:43:43 +0300

    feat: implement generic Repository base class in Data project

---
+++ FreelanceManager.Data/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+using FreelanceManager.Core.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreelanceManager.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
