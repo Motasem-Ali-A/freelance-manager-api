b8593840dd9d0f764a113123f3a75060b33c975a
Motasem-Ali-A <mutasem.abusarhan@gmail.com>
2026-03-12 00:09:51 +0300

    chore: register repositories in dependency injection container

--- FreelanceManager.API/Program.cs
+++ FreelanceManager.API/Program.cs
@@ -1,4 +1,6 @@
+using FreelanceManager.Core.interfaces;
 using FreelanceManager.Data;
+using FreelanceManager.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<ITimeEntryRepository, TimeEntryRepository>();
+builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
 
 var app = builder.Build();
 
