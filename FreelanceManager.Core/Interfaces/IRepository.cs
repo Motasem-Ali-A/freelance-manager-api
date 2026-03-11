c69acffa611bd3f323e63e3929fcda77f0fcc8ef
Motasem-Ali-A <mutasem.abusarhan@gmail.com>
2026-03-11 20:06:37 +0300

    feat: add generic IRepository interface to Core

---
+++ FreelanceManager.Core/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+namespace FreelanceManager.Core.interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveChangesAsync();
+    }
+}
