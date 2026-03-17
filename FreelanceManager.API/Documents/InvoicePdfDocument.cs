45a18a4cfc056346a1a1df5a8b9cc24696596558
Motasem-Ali-A <mutasem.abusarhan@gmail.com>
2026-03-17 23:58:50 +0300

    feat: add invoice PDF items table

--- FreelanceManager.API/Documents/InvoicePdfDocument.cs
+++ FreelanceManager.API/Documents/InvoicePdfDocument.cs
@@ -1,4 +1,5 @@
 using FreelanceManager.Core.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using QuestPDF.Fluent;
 using QuestPDF.Infrastructure;
 
@@ -49,13 +50,47 @@ namespace FreelanceManager.API.Documents
         {
             container.Row(row =>
             {
-                //Business info
+                //Bill To section
                 row.RelativeItem().Column(column =>
                 {
                     column.Item().Text("Bill To: ").FontSize(20).Bold();
                     column.Item().Text(_invoice.Client.Name);
                     column.Item().Text(_invoice.Client.Email);
+
+                    column.Item().PaddingTop(20);
+                    // Items table
+                    column.Item().Element(ComposeTable);
                 });
+
+            });
+        }
+
+        private void ComposeTable(IContainer container)
+        {
+            container.Table(table =>
+            {
+                table.ColumnsDefinition(columns =>
+                {
+                    columns.RelativeColumn(3); // Description
+                    columns.RelativeColumn(1); // Quantity
+                    columns.RelativeColumn(1); // Unit Price
+                    columns.RelativeColumn(1); // Total
+                });
+                table.Header(header =>
+                {
+                    header.Cell().Text("Description").Bold();
+                    header.Cell().Text("Qty").Bold();
+                    header.Cell().Text("Unit Price").Bold();
+                    header.Cell().Text("Total").Bold();
+                });
+
+                foreach (var item in _invoice.InvoiceItems)
+                {
+                    table.Cell().Text(item.Description);
+                    table.Cell().Text(item.Quantity.ToString());
+                    table.Cell().Text($"${item.UnitPrice}");
+                    table.Cell().Text($"${item.Total}");
+                }
             });
         }
         private void ComposeFooter(IContainer container) { }
